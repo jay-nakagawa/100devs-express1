d33b8e7e3ae501a13a633099c9a21bb6935f2d1e
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2022-12-07 15:57:26 -0500

    1

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'test'
 
-MongoClient.connect("mongodb+srv://jaynakagawa777:mongod123@cluster0.t6yfhjw.mongodb.net/?retryWrites=true&w=majority", { useUnifiedTopology: true })
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
