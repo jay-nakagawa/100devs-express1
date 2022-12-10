464e3187d171cdf2302f6b3411a95ee04b020f4d
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2022-12-10 17:19:34 -0500

    fubar

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const MongoClient = require('mongodb').MongoClient
 const PORT = process.env.PORT || 3000
+// const client = new MongoClient(connectDB)
+
 require('dotenv').config()
 
 app.set('view engine', 'ejs')
@@ -13,73 +16,41 @@ app.use(express.json())
 
 
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
-  
-let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'test'
-
-app.get('/',(request, response)=>{
-    db.collection('rappers').find().sort({likes: -1}).toArray()
-    .then(data => {
-        response.render('index.ejs', { info: data })
-    })
-    .catch(error => console.error(error))
-})
-
-app.post('/addRapper', (request, response) => {
-    db.collection('rappers').insertOne({stageName: request.body.stageName,
-    birthName: request.body.birthName, likes: 0})
-    .then(result => {
-        console.log('Rapper Added')
-        response.redirect('/')
-    })
-    .catch(error => console.error(error))
-})
-
-app.put('/addOneLike', (request, response) => {
-    db.collection('rappers').updateOne({stageName: request.body.stageNameS, birthName: request.body.birthNameS,likes: request.body.likesS},{
-        $set: {
-            likes:request.body.likesS + 1
-          }
-    },{
-        sort: {_id: -1},
-        upsert: true
-    })
-    .then(result => {
-        console.log('Added One Like')
-        response.json('Like Added')
-    })
-    .catch(error => console.error(error))
-
-})
-
-app.delete('/deleteRapper', (request, response) => {
-    db.collection('rappers').deleteOne({stageName: request.body.stageNameS})
-    .then(result => {
-        console.log('Rapper Deleted')
-        response.json('Rapper Deleted')
-    })
-    .catch(error => console.error(error))
-
-})
-    .catch(console.error)
 
 
 
-})
-    
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect("mongodb+srv://jaynakagawa777:mongod123@cluster0.t6yfhjw.mongodb.net/?retryWrites=true&w=majority");
+        console.log(`MongoDB Connected!!!`);
+        
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
 
 
 connectDB().then(() => {
     app.listen(PORT, () => {
-        console.log("listening for requests");
+        console.log(`Listening on port ${PORT}`);
     })
+    
+    
+ 
 })
 
-// app.listen(process.env.PORT || PORT, ()=>{
-//     console.log(`Server running on port ${PORT}`)
-// })
+
+let db,
+    // dbConnectionStr = process.env.DB_STRING,
+    dbName = 'test'
+
+    app.get("/", async (req, res) => {
+        
+        let item = await conn.db("my_db")
+                    .collection("my_collection")
+                    .findOne({my_item: my_item})
+    
+        return res.json(item)
+    })
+
